79c9d3c8f29d332b464fbec0a61f6e514b557b83
monifra <monia.frankowska@gmail.com>
2019-11-18 21:52:41 +0100

    updating css
    adding styles to css

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
 const $inputName = $('#name');
 $inputName.focus(); //focusing on a first input after page load
+const $inputOtherJobRole = $('#other-title');
+$inputOtherJobRole.hide(); //hidding other job input field
 
