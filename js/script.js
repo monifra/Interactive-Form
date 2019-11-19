c34b080ecb71cea3b9e3018f4ee7a10bc7a34561
monifra <monia.frankowska@gmail.com>
2019-11-19 21:58:35 +0100

    Update script.js
    manipulating t-shirt section

--- js/script.js
+++ js/script.js
@@ -3,3 +3,25 @@ $inputName.focus(); //focusing on a first input after page load
 const $inputOtherJobRole = $('#other-title');
 $inputOtherJobRole.hide(); //hidding other job input field
 
+//hide select theme in a drop down menu
+const $selectThemeOption = $('#design>option:first-child');
+$selectThemeOption.attr('disabled', 'true').attr('hidden', 'true');
+// hide the colors in a drop down menu
+const $colors = $('#color>option');
+$colors.attr('disabled', 'true').attr('hidden', 'true');
+// update color field to "Please select a T-shirt theme"
+$('#color').val('textbox');
+$('#color').prepend('<option value="selectTheme">Please select a T-shirt</option>');
+//create change event listener on a design menu
+// if the user select JS Puns 
+// then display cornflower blue dark slate grey and gold
+// else if the user select I love JS
+// then display tomato steel blue and dim grey
+
+
+
+
+
+//hide the 'Select Payment Method' option from drop down menu
+const $selectPaymentMethod = $('#payment>option[value="select method"]');
+$selectPaymentMethod.attr('disabled', 'true').attr('hidden', 'true');
