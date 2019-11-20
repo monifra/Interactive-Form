f9820571e789bf90e558e29fa3d162a3aa67ae12
monifra <monia.frankowska@gmail.com>
2019-11-20 20:06:34 +0100

    update js T-shirt Section

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
 const $inputName = $('#name');
 $inputName.focus(); //focusing on a first input after page load
+
+//OTHER JOB SECTION
+
 const $inputOtherJobRole = $('#other-title');
 $inputOtherJobRole.hide(); //hidding other job input field
 
+//T-SHIRT SECTION
 //hide select theme in a drop down menu
 const $selectThemeOption = $('#design>option:first-child');
 $selectThemeOption.attr('disabled', 'true').attr('hidden', 'true');
@@ -20,25 +24,33 @@ const $designSelectMenu = $('#design');
 
 $designSelectMenu.change(function(event){
     console.log($(event.target).val());
+    $('#color option[value="selectTheme"]').attr('disabled', 'true').attr('hidden', 'true').removeAttr('selected');
     $colors.each(function(i){
-        if($(event.target).val() === 'js puns') { // if the user select JS Puns
+        if($(event.target).val() === 'js puns') { // if the user select JS Puns than display cornflowerblue, darkslategrey,gold
             console.log('hi i\'m working');
-            $('#color option[value="cornflowerblue"]').removeAttr('disabled').removeAttr('hidden');
+            
+            $('#color option[value="cornflowerblue"]').removeAttr('disabled').removeAttr('hidden').prop('selected', 'true');
             $('#color option[value="darkslategrey"]').removeAttr('disabled').removeAttr('hidden');
-            $('#color option[value="gold"]').removeAttr('disabled').removeAttr('hidden');  
+            $('#color option[value="gold"]').removeAttr('disabled').removeAttr('hidden');
+            $('#color option[value="tomato"]').attr('disabled', 'true').attr('hidden', 'true').removeAttr('selected');
+            $('#color option[value="steelblue"]').attr('disabled', 'true').attr('hidden', 'true');
+            $('#color option[value="dimgrey"]').attr('disabled', 'true').attr('hidden', 'true');
+            
         }
-        if($(event.target).val() === 'heart js') { // if the user select JS Puns
+        if($(event.target).val() === 'heart js') { // else if the user select I love JS then display tomato steel blue and dim grey
             console.log('hi i\'m working');
-            $('#color option[value="tomato"]').removeAttr('disabled').removeAttr('hidden');
+            
+            $('#color option[value="tomato"]').removeAttr('disabled').removeAttr('hidden').prop('selected', 'true');
             $('#color option[value="steelblue"]').removeAttr('disabled').removeAttr('hidden');
-            $('#color option[value="dimgrey"]').removeAttr('disabled').removeAttr('hidden');  
+            $('#color option[value="dimgrey"]').removeAttr('disabled').removeAttr('hidden');
+            $('#color option[value="cornflowerblue"]').attr('disabled', 'true').attr('hidden', 'true').removeAttr('selected');
+            $('#color option[value="darkslategrey"]').attr('disabled', 'true').attr('hidden', 'true');
+            $('#color option[value="gold"]').attr('disabled', 'true').attr('hidden', 'true');
         }
-        
-        // else if the user select I love JS
-        // then display tomato steel blue and dim grey
-
     });
 });
+
+//ACTIVITY SECTION
 
 
 
