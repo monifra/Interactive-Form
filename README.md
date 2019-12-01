d7162c60509b3d2ef6e3d171a62c82207d5dc9c2
monifra <monia.frankowska@gmail.com>
2019-12-01 20:21:40 +0100

    major changes js
    major structural changes js
    new info in read me

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# Interactive-Form
+Techdegree Project 3 Interactive_Form
+
+Interactive Form Project is an interactive registration form for tech conference. In this project I'm using jQuery library.
+Form is using some customise behaviours, such as hiding unnecessary input fields or unnecessary select options. 
+All of the validations for this form are powered by javaScript and provide helpful informations when the users informations are invalid.
+I'm using Progressive Enhancement technique to make this form work, even if the user has enabled javaScript.
+
+When user visit the page it automatically focus on a first name input. Then there is a job section, when the user selects other job option, the input for other job appears. In the t-shirt section form allows you to select only t-shirt colour that is possible in your selected t-shirt theme. Until theme is selected you can't select the colour. In activities section you have to choose at least one activity, form automatically disabled activities that are scheduled at the same time as your already selected activity. The last section is payment. As a default it is set to show Credit Card as payment option. You can change payment option to PyPal or Bitcoin and you will see payment information for chosen payment option. When you click submit button and some information are invalid you will see error messages. When you try to submit an empty form you will see an error message for all required fields.
+
+I've decided to work with provided CSS and make it my own. I've worked with fonts, colours, backgrounds and input transitions.
+
+Used techniques:
+
+- Progressive Enhancement,
+- Unobtrusive JavaScript,
+- jQuery Library,
+- Regular Expressions
  
--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+/*************
+ Techdegree Project 3 Interactive_Form
+ *************/
+//Interactive Form Project is an interactive registration form for tech conference.
 /***
 VARIABLES
 ***/
@@ -17,131 +21,29 @@ const $designSelectMenu = $('#design'); //choose design select element
 let totalCost = 0; //variable that will hold the total cost of users choosen activities
 const $appendCost = $('.activities').append('<p class="cost">Total: $'+ totalCost +'</p>'); //appending to a page cost paragraph
 const $activitiesParent =$('.activities');
-/***
-OTHER JOB SECTION EVENT HANDLER
-***/
-$jobSelectMenu.change(function(event){ //if there is a change in job select menu
-    $jobs.each(function(i){
-        if($(event.target).val() === 'other'){ //if other option selected
-            $inputOtherJobRole.removeAttr('hidden'); //show other job input 
-        }else{
-            $inputOtherJobRole.attr('hidden', 'true'); //else hide other job input
-        }
-    });
-});
-
-/***
-T-SHIRT SECTION EVENT HANDLER
-***/
-$designSelectMenu.change(function(event){ //create change event listener on a design menu
-    console.log($(event.target).val());
-    $('#color option[value="selectTheme"]').attr('disabled', 'true').attr('hidden', 'true').removeAttr('selected');
-    $colors.each(function(i){
-        if($(event.target).val() === 'js puns') { // if the user select JS Puns than display cornflowerblue, darkslategrey,gold
-            
-            $('#color option[value="cornflowerblue"]').removeAttr('disabled').removeAttr('hidden').prop('selected', 'true');
-            $('#color option[value="darkslategrey"]').removeAttr('disabled').removeAttr('hidden');
-            $('#color option[value="gold"]').removeAttr('disabled').removeAttr('hidden');
-            $('#color option[value="tomato"]').attr('disabled', 'true').attr('hidden', 'true').removeProp('selected');
-            $('#color option[value="steelblue"]').attr('disabled', 'true').attr('hidden', 'true');
-            $('#color option[value="dimgrey"]').attr('disabled', 'true').attr('hidden', 'true');
-            
-        }
-        if($(event.target).val() === 'heart js') { // else if the user select I love JS then display tomato steel blue and dim grey
-            
-            $('#color option[value="tomato"]').removeAttr('disabled').removeAttr('hidden').prop('selected', 'true');
-            $('#color option[value="steelblue"]').removeAttr('disabled').removeAttr('hidden');
-            $('#color option[value="dimgrey"]').removeAttr('disabled').removeAttr('hidden');
-            $('#color option[value="cornflowerblue"]').attr('disabled', 'true').attr('hidden', 'true').removeProp('selected');
-            $('#color option[value="darkslategrey"]').attr('disabled', 'true').attr('hidden', 'true');
-            $('#color option[value="gold"]').attr('disabled', 'true').attr('hidden', 'true');
-        }
-    });
-});
-
-/*** 
-ACTIVITY SECTION EVENT HANDLER
- ***/
-$activitiesParent.change(function(event){  //listen for a change in an activity section
-    const $activitiesCheckboxClicked = $(event.target); //selecting input elements in activities
-    const $checkedCost = $activitiesCheckboxClicked.attr('data-cost');
-    const $newCheckedCost = $checkedCost.replace(/[^0-9]/, ''); //every character that is not a number will be removed from a string
-    const $newCheckedCostNumber = parseFloat($newCheckedCost); //change variable to a number
-    const $activitiesTime = $activitiesCheckboxClicked.attr('data-day-and-time');
-    const $checkbox = $('.activities input');
-    let $costText = $('.cost'); //selecting p that is holding cost value
-
-    if($activitiesCheckboxClicked.prop('checked')){ // if checkbox is checked
-        totalCost += $newCheckedCostNumber; // add the cost
-        $costText.text('Total: $' + totalCost); //display the cost
-    }else{ // checkbox not checked
-        totalCost -= $newCheckedCostNumber; // substract the cost
-        $costText.text('Total: $' + totalCost);  //display the cost 
-    }
-
-    $checkbox.each(function(i){ //looping over all of the inputs and disabled this which happens at the same time that those which were chosen
-        const $currentActivity = $checkbox[i];
-
-        if($($currentActivity).attr('data-day-and-time') === $activitiesTime && $($activitiesCheckboxClicked).attr('name') !== $($currentActivity).attr('name')){
-            if($activitiesCheckboxClicked.prop('checked')){
-                $($currentActivity).attr('disabled', true);
-                $($currentActivity).parent().css('color', '#dddfe0');
-            }else{
-                $($currentActivity).removeAttr('disabled');
-                $($currentActivity).parent().css('color', 'black');
-            }
-        }
-    });
-});
-/***
-PAYMENT INFORMATION SECTION
- ***/
-
+//payment section
 const $selectPaymentMethod = $('#payment>option[value="select method"]').attr('disabled', 'true').attr('hidden', 'true'); //hide the 'Select Payment Method' option from drop down menu
 const $creditCard = $('#credit-card');
-const $payPal = $('#paypal');
-const $bitcoin = $('#bitcoin');
-
+const $payPal = $('#paypal').attr('hidden', 'true');//chose paypal and hide paypal payment info
+const $bitcoin = $('#bitcoin').attr('hidden', 'true');// chose bitcoin and hide bitcoin payment info
 const $selectedPaymentValue=$('#payment').val('Credit Card');  //set credit card as selected value
-
-$payPal.attr('hidden', 'true'); //hide paypal payment info
-$bitcoin.attr('hidden', 'true'); //hide bitcoin payment info
-
-const $paymentSelectMenu = $('#payment');
-
-$paymentSelectMenu.change(function(event){  //change event handler that shows and hides informations about chosen payment option
-    console.log($(event.target).val());
-
-        if($(event.target).val() === 'Credit Card') { //when credit card payment option is chosen
-
-            $creditCard.removeAttr('hidden').prop('selected', 'true'); //show credit card inputs 
-            $payPal.attr('hidden', 'true'); //hide paypal info
-            $bitcoin.attr('hidden', 'true'); //hide bitcoin info
-        }
-        if($(event.target).val() === 'PayPal') { //when paypal payment option is chosen
-            
-            $payPal.removeAttr('hidden').prop('selected', 'true');;
-            $creditCard.attr('hidden', 'true');
-            $bitcoin.attr('hidden', 'true');
-            
-        }
-        if($(event.target).val() === 'Bitcoin'){ //when bitcoin payment option is chosen
-            $bitcoin.removeAttr('hidden').prop('selected', 'true');;
-            $creditCard.attr('hidden', 'true');
-            $payPal.attr('hidden', 'true');
-        }
-
-});
-/*** 
-FORM VALIDATION & VALIDATION MESSAGES
-***/
-
+const $paymentSelectMenu = $('#payment'); //selects payment menu
+//form validation
 //adding validators messages to a page
 const $nameInput = $('#name');
 const $nameInputError = $('<p class="error">Name field can\'t be empty</p>').insertAfter($nameInput).hide(); //error message for name input
 const $emailInputError = $('<p class="error">Must be a valid email address</p>').insertAfter($('#mail')).hide(); //error message for email input
 const $activitiesError = $('<p class="error">Choose at least one activity</p>').insertAfter($('.activities legend')).hide(); //error message for activity section
-
+//adding optional validators messages to a page
+const $cardNumberError = $('<p class="error">Credit card number must have between 13 and 16 numbers</p>').insertAfter($('#cc-num')).hide();
+const $zipError = $('<p class="error">Zip Code must be 5-digit number</p>').insertAfter($('#zip')).hide();
+const $cvvError = $('<p class="error">CVV must be exactly 3 digits long</p>').insertAfter($('#cvv')).hide();
+//form submit
+const $form = $('form');
+/***
+FUNCTIONS
+***/
+//FORM VALIDATION & VALIDATION MESSAGES
 const $validateName = () =>{ //name validation
     if($($nameInput).val()===''){ //if ther isn't a value in the name input
         $nameInputError.show();
@@ -178,12 +80,7 @@ const $validateActivity = () =>{ //activity validation
         return false;
     }
 };
-/***
- ONLY VALIDATED IF THE PAYMENT METHOD IS "CREDIT CARD"
- ***/
-const $cardNumberError = $('<p class="error">Credit card number must have between 13 and 16 numbers</p>').insertAfter($('#cc-num')).hide();
-const $zipError = $('<p class="error">Zip Code must be 5-digit number</p>').insertAfter($('#zip')).hide();
-const $cvvError = $('<p class="error">CVV must be exactly 3 digits long</p>').insertAfter($('#cvv')).hide();
+//ONLY VALIDATED IF THE PAYMENT METHOD IS "CREDIT CARD"
 const $validateCreditCardNumber = () =>{ //credit card validation
     const $cardInputVal = $('#cc-num').val();
     const isValid = /^\d{13,16}$/.test($cardInputVal); //test if value from card input is a number between 13-16 numbers
@@ -217,10 +114,7 @@ const $validateCVV = ()=>{ //CVV validation
         return false;
     }
 };
-/***
-MASTER VALIDATORS
- ***/
-
+//MASTER VALIDATORS
 //function checking credit card validators
 const $validateCreditCard=()=>{
     if($($paymentSelectMenu).val() === 'Credit Card' && $validateCreditCardNumber() === false){
@@ -239,20 +133,112 @@ const $validateEmpty = () => {
         return false;
     } else{
         return true;
-    }
-    
+    }    
 };
 //function handling invalid form
 const $validateAll = () => {
     if($validateName() === false || $validateEmail() === false || $validateActivity() === false || $validateCreditCard()=== false){
         return false;
-    }
-    
+    }  
 };
 /***
-FORM SUBMIT EVENT HANDLER
+ EVENT HANDLERS 
  ***/
-const $form = $('form');
+//OTHER JOB SECTION EVENT HANDLER
+$jobSelectMenu.change(function(event){ //if there is a change in job select menu
+    $jobs.each(function(i){
+        if($(event.target).val() === 'other'){ //if other option selected
+            $inputOtherJobRole.removeAttr('hidden'); //show other job input 
+        }else{
+            $inputOtherJobRole.attr('hidden', 'true'); //else hide other job input
+        }
+    });
+});
+//T-SHIRT SECTION EVENT HANDLER
+$designSelectMenu.change(function(event){ //create change event listener on a design menu
+    console.log($(event.target).val());
+    $('#color option[value="selectTheme"]').attr('disabled', 'true').attr('hidden', 'true').removeAttr('selected');
+    $colors.each(function(i){
+        if($(event.target).val() === 'js puns') { // if the user select JS Puns than display cornflowerblue, darkslategrey,gold
+            
+            $('#color option[value="cornflowerblue"]').removeAttr('disabled').removeAttr('hidden').prop('selected', 'true');
+            $('#color option[value="darkslategrey"]').removeAttr('disabled').removeAttr('hidden');
+            $('#color option[value="gold"]').removeAttr('disabled').removeAttr('hidden');
+            $('#color option[value="tomato"]').attr('disabled', 'true').attr('hidden', 'true').removeProp('selected');
+            $('#color option[value="steelblue"]').attr('disabled', 'true').attr('hidden', 'true');
+            $('#color option[value="dimgrey"]').attr('disabled', 'true').attr('hidden', 'true');
+            
+        }
+        if($(event.target).val() === 'heart js') { // else if the user select I love JS then display tomato steel blue and dim grey
+            
+            $('#color option[value="tomato"]').removeAttr('disabled').removeAttr('hidden').prop('selected', 'true');
+            $('#color option[value="steelblue"]').removeAttr('disabled').removeAttr('hidden');
+            $('#color option[value="dimgrey"]').removeAttr('disabled').removeAttr('hidden');
+            $('#color option[value="cornflowerblue"]').attr('disabled', 'true').attr('hidden', 'true').removeProp('selected');
+            $('#color option[value="darkslategrey"]').attr('disabled', 'true').attr('hidden', 'true');
+            $('#color option[value="gold"]').attr('disabled', 'true').attr('hidden', 'true');
+        }
+    });
+}); 
+//ACTIVITY SECTION EVENT HANDLER
+$activitiesParent.change(function(event){  //listen for a change in an activity section
+    const $activitiesCheckboxClicked = $(event.target); //selecting input elements in activities
+    const $checkedCost = $activitiesCheckboxClicked.attr('data-cost');
+    const $newCheckedCost = $checkedCost.replace(/[^0-9]/, ''); //every character that is not a number will be removed from a string
+    const $newCheckedCostNumber = parseFloat($newCheckedCost); //change variable to a number
+    const $activitiesTime = $activitiesCheckboxClicked.attr('data-day-and-time');
+    const $checkbox = $('.activities input');
+    let $costText = $('.cost'); //selecting p that is holding cost value
+
+    if($activitiesCheckboxClicked.prop('checked')){ // if checkbox is checked
+        totalCost += $newCheckedCostNumber; // add the cost
+        $costText.text('Total: $' + totalCost); //display the cost
+    }else{ // checkbox not checked
+        totalCost -= $newCheckedCostNumber; // substract the cost
+        $costText.text('Total: $' + totalCost);  //display the cost 
+    }
+
+    $checkbox.each(function(i){ //looping over all of the inputs and disabled this which happens at the same time that those which were chosen
+        const $currentActivity = $checkbox[i];
+
+        if($($currentActivity).attr('data-day-and-time') === $activitiesTime && $($activitiesCheckboxClicked).attr('name') !== $($currentActivity).attr('name')){
+            if($activitiesCheckboxClicked.prop('checked')){
+                $($currentActivity).attr('disabled', true);
+                $($currentActivity).parent().css('color', '#dddfe0');
+            }else{
+                $($currentActivity).removeAttr('disabled');
+                $($currentActivity).parent().css('color', 'black');
+            }
+        }
+    });
+});
+//PAYMENT INFORMATION SECTION EVENT HANDLER
+$paymentSelectMenu.change(function(event){  //change event handler that shows and hides informations about chosen payment option
+    console.log($(event.target).val());
+
+        if($(event.target).val() === 'Credit Card') { //when credit card payment option is chosen
+
+            $creditCard.removeAttr('hidden').prop('selected', 'true'); //show credit card inputs 
+            $payPal.attr('hidden', 'true'); //hide paypal info
+            $bitcoin.attr('hidden', 'true'); //hide bitcoin info
+        }
+        if($(event.target).val() === 'PayPal') { //when paypal payment option is chosen
+            
+            $payPal.removeAttr('hidden').prop('selected', 'true');;
+            $creditCard.attr('hidden', 'true');
+            $bitcoin.attr('hidden', 'true');
+            
+        }
+        if($(event.target).val() === 'Bitcoin'){ //when bitcoin payment option is chosen
+            $bitcoin.removeAttr('hidden').prop('selected', 'true');;
+            $creditCard.attr('hidden', 'true');
+            $payPal.attr('hidden', 'true');
+        }
+
+});
+/***
+FORM SUBMIT MASTER EVENT HANDLER
+ ***/
 //Event handler that prevents submitting the form if it's not correct
 $form.submit(function(event){ 
     if($validateEmpty() === false){ //prevent submission when requirements were not met
